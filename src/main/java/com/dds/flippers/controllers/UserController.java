7ff2c571c1b1b447e3840e9ec3405a7da7fb6496
cowichiwi <cowichiwo@gmail.com>
2025-07-03 06:22:47 +0000

    nosotros

--- src/main/java/com/dds/flippers/controllers/UserController.java
+++ src/main/java/com/dds/flippers/controllers/UserController.java
@@ -24,6 +24,11 @@ public class UserController {
         return "client/register";
     }
 
+    @GetMapping("/nosotros")
+    public String showNosotros() {
+        return "client/nosotros";
+    }
+
     // Crear nuevo usuario
     @PostMapping("/register")
     public String createUser(@ModelAttribute UserModel userModel) {
