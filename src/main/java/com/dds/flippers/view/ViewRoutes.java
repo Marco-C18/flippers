2ed9340257b7ea8ae0591dc02ac0c5946b5533a0
marco <zmarco.20.ac@gmail.com>
2025-07-03 11:12:28 -0500

    Corregir error fantasma

--- src/main/java/com/dds/flippers/view/ViewRoutes.java
+++ src/main/java/com/dds/flippers/view/ViewRoutes.java
@@ -1,7 +1,9 @@
 package com.dds.flippers.view;
 
 public class ViewRoutes {
+
     // Clase representativa para representar VIEW mediante rutas
     public static final String HOME = "home";
     public static final String ABOUT_US = "nosotros";
+    
 }
